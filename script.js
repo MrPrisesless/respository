0373fef3228690ec5784be8c73c2230e8781e0ee
MrPrisesless <minecraftdemateoel@gmail.com>
2024-05-20 16:14:45 -0400

    variables con operaciones matematicas

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hola mundo!')
+// crear 4 variables, que indiquen suma, resta, multiplicacion y division
+let manzana = 4
+console.log('el valor de manzana es = ' + manzana)
+manzana = manzana-2
+console.log('y el valor de manzana si le restamos 2 es  = ' + manzana)
+
+let mario = 69
+console.log('el valor de mario es = ' + mario)
+mario = mario*2
+console.log('y el valor de mario si lo multiplicamos por 2 es = ' + mario)
+
+let huevos = 23
+console.log('el valor de huevos es = ' + huevos)
+huevos = huevos+34
+console.log('el valor de huevos si le sumamos 34 es = ' + huevos)
+
+let atributo = 56
+console.log('el valor de atributo es = ' + atributo)
+atributo = atributo/2
+console.log('y si el valor de atributo lo dividimos a la mitad es = ' + atributo)
