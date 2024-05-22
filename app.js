e543cda7de496463022cfcc0fd820cc6c9158f7f
MrPrisesless <minecraftdemateoel@gmail.com>
2024-05-22 16:04:00 -0400

    actualizad

--- app.js
+++ app.js
@@ -17,5 +17,65 @@ console.log(manzana)
 manzana = manzana+1
 console.log(manzana)
 
+//esta variable es suma :v
+let omgEstoVaASerEpicoPapus = 1 + 9999
+console.log(omgEstoVaASerEpicoPapus)
+
+//esta variable es multiplicacion
+let multiplicacion = 50 * Number.MAX_SAFE_INTEGER
+console.log(multiplicacion)
+
+//esto es un modulo
+let modulo = 2000 % 100
+console.log(modulo)
+
+// Operadores Lógicos: hoy SI (&&) mañana NO (||). < (Menor que), > (Mayor que)
+
+//Esta variable es Menor Que (<)
+
+let menorQue = 2 < 1
+console.log(menorQue)
+
+//Esta variable es Mayor Que (>)
+
+let mayorQue = 2 > 1
+console.log (mayorQue)
+// Esta variable es Igual (=)
+
+let igual = 2 == 2
+console.log(igual)
+//Esta variable es Menor O Igual Que (<=)
+
+let menorOIgualQue = 2 <= 3
+console.log (menorOIgualQue)
+
+//Esta variable es Mayor O  Igual Que (>=)
+
+let mayorOIgualQue = 5 >= 1
+console.log (menorOIgualQue)
+
+//Esta variabnle es SI y NO
+
+let y = 2<3 && 5<10
+console.log(y)
+
+let n = 5<10 || 10<5
+console.log(n)
+
+//Condicional If
+
+if (2>1) {
+    let si = "siEsCiertoMiGente"
+    console.log(si)
+    
+}
+
+if (2<1) {
+    console.log('Es mayor')
+    
+} else {
+    console.log('NUH UH')
+}
+
 
 
