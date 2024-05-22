88ccdc8cea63fb6ed2e7270a14c312865fe9eafd
MrPrisesless <minecraftdemateoel@gmail.com>
2024-05-22 15:05:59 -0400

    skibidi

---
+++ app.js
@@ -0,0 +1,21 @@
+// esta variable es de tipo string banda
+let variable = "hoy si mañana no"
+console.log(variable)
+
+// esta variable es tipo boileado
+let verdadero = true
+
+// esta variable es de tipo undefined
+let indefinido = undefined
+
+// esta variable es de tipo nulo
+let nulo = null
+
+//variable fina
+let manzana = 10
+console.log(manzana)
+manzana = manzana+1
+console.log(manzana)
+
+
+
